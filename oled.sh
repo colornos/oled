5b2b3fe5db9d263e5602b60811aee34db0fb8b44
colornos <rivas432@hotmail.com>
2023-05-29 00:47:40 -0500

    Update oled.sh

--- oled.sh
+++ oled.sh
@@ -55,7 +55,7 @@ else
 
     print("Hello blinka!")
 
-    # Try to great a Digital input
+    # Try to create a Digital input
     pin = digitalio.DigitalInOut(board.D4)
     print("Digital IO ok!")
 
@@ -68,7 +68,7 @@ else
     print("SPI ok!")
 
     print("done!")
-    EOF
+EOF
 
     # Create oled_test.py file
     cat << EOF > oled_test.py
@@ -78,49 +78,48 @@ else
     import busio
     from PIL import Image, ImageDraw, ImageFont
     import adafruit_ssd1306
-i2c = busio.I2C(SCL, SDA)
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
-disp.fill(0)
-disp.show()
-
-width = disp.width
-height = disp.height
-image = Image.new("1", (width, height))
-
-draw = ImageDraw.Draw(image)
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-padding = -2
-top = padding
-bottom = height - padding
-x = 0
-
-font = ImageFont.load_default()
-
-while True:
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'cut -f 1 -d " " /proc/loadavg'
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
-    draw.text((x, top + 16), MemUsage, font=font, fill=255)
-    draw.text((x, top + 25), Disk, font=font, fill=255)
-
-    disp.image(image)
+    i2c = busio.I2C(SCL, SDA)
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+    #... rest of your Python code ...
+    disp.fill(0)
     disp.show()
+
+    width = disp.width
+    height = disp.height
+    image = Image.new("1", (width, height))
+
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    padding = -2
+    top = padding
+    bottom = height - padding
+    x = 0
+
+    font = ImageFont.load_default()
+
+    while True:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        cmd = "hostname -I | cut -d' ' -f1"
+        IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = 'cut -f 1 -d " " /proc/loadavg'
+        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB %s", $3,$2,$5}\''
+        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+        draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
+        draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+        draw.text((x, top + 16), MemUsage, font=font, fill=255)
+        draw.text((x, top + 25), Disk, font=font, fill=255)
+
+        disp.image(image)
+        disp.show()
+        time.sleep(0.1)
     time.sleep(0.1)
 EOF
-
-echo "Script finished successfully"
-
-    EOF
 
     echo "Script finished successfully"
 
