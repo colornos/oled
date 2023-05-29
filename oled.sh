8a64e9c5f3524db88bdfad93510cb7bf3a6063d1
colornos <rivas432@hotmail.com>
2023-05-29 01:21:42 -0500

    Update oled.sh

--- oled.sh
+++ oled.sh
@@ -1,127 +1,107 @@
 #!/bin/bash
 
-# Define a flag file
-FLAG_FILE="/home/pi/oled/.flag_script"
+# Update and upgrade the Pi
+sudo apt-get update
+sudo apt-get upgrade
 
-# Install and update
-if [ ! -f "$FLAG_FILE" ]; then
+# Create blinkatest.py file
+cat << EOF > blinkatest.py
+import board
+import digitalio
+import busio
 
-    # Update and upgrade the Pi
-    sudo apt-get update
-    sudo apt-get upgrade
+print("Hello blinka!")
 
-    # Install python3-pip if not installed
-    if ! command -v pip3 &> /dev/null
-    then
-        sudo apt-get install python3-pip
-    fi
+# Try to great a Digital input
+pin = digitalio.DigitalInOut(board.D4)
+print("Digital IO ok!")
 
-    # Install adafruit-circuitpython-ssd1306
-    sudo pip3 install adafruit-circuitpython-ssd1306
+# Try to create an I2C device
+i2c = busio.I2C(board.SCL, board.SDA)
+print("I2C ok!")
 
-    # Install python3-pil
-    sudo apt-get install python3-pil
+# Try to create an SPI device
+spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+print("SPI ok!")
 
-    # Upgrade setuptools
-    sudo pip3 install --upgrade setuptools
+print("done!")
+EOF
 
-    # Change directory to home
-    cd ~
+# Create oled_test.py file
+cat << EOF > oled_test.py
+import time
+import subprocess
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
-    # Install adafruit-python-shell
-    sudo pip3 install --upgrade adafruit-python-shell
+i2c = busio.I2C(SCL, SDA)
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
-    # Create blinkatest.py file
-    cat << BLINKA > blinkatest.py
-    import board
-    import digitalio
-    import busio
+disp.fill(0)
+disp.show()
 
-    print("Hello blinka!")
+width = disp.width
+height = disp.height
+image = Image.new("1", (width, height))
 
-    # Try to great a Digital input
-    pin = digitalio.DigitalInOut(board.D4)
-    print("Digital IO ok!")
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # Try to create an I2C device
-    i2c = busio.I2C(board.SCL, board.SDA)
-    print("I2C ok!")
+padding = -2
+top = padding
+bottom = height - padding
+x = 0
 
-    # Try to create an SPI device
-    spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
-    print("SPI ok!")
+font = ImageFont.load_default()
 
-    print("done!")
-BLINKA
-
-    # Create oled_test.py file
-    cat << OLED > oled_test.py
-    import time
-    import subprocess
-    from board import SCL, SDA
-    import busio
-    from PIL import Image, ImageDraw, ImageFont
-    import adafruit_ssd1306
-
-    i2c = busio.I2C(SCL, SDA)
-    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-
-    disp.fill(0)
-    disp.show()
-
-    width = disp.width
-    height = disp.height
-    image = Image.new("1", (width, height))
-
-    draw = ImageDraw.Draw(image)
+while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'cut -f 1 -d " " /proc/loadavg'
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB %s", $3,$2,$5}\''
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    padding = -2
-    top = padding
-    bottom = height - padding
-    x = 0
+    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
+    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+    draw.text((x, top + 16), MemUsage, font=font, fill=255)
+    draw.text((x, top + 25), Disk, font=font, fill=255)
 
-    font = ImageFont.load_default()
+    disp.image(image)
+    disp.show()
+    time.sleep(0.1)
+EOF
 
-    while True:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        cmd = "hostname -I | cut -d' ' -f1"
-        IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'cut -f 1 -d " " /proc/loadavg'
-        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB %.2f%%\", $3,$2,$3*100/$2 }'"
-        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB %s", $3,$2,$5}\''
-        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-        draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-        draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
-        draw.text((x, top + 16), MemUsage, font=font, fill=255)
-        draw.text((x, top + 25), Disk, font=font, fill=255)
-
-        disp.image(image)
-        disp.show()
-        time.sleep(0.1)
-OLED
-
-    # Download the raspi-blinka.py script
-    wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-
-    # Create a flag file
-    touch "$FLAG_FILE"
-
-    # Run the script
-    sudo python3 raspi-blinka.py
-
-    # Reboot
-    sudo reboot
-else
-
-    # Check I2C and SPI devices
-    ls /dev/i2c* /dev/spi*
-
-    echo "Script finished successfully"
-
-    # Remove flag file
-    rm "$FLAG_FILE"
+# Install python3-pip if not installed
+if ! command -v pip3 &> /dev/null
+then
+    sudo apt-get install python3-pip
 fi
+
+# Install adafruit-circuitpython-ssd1306
+sudo pip3 install adafruit-circuitpython-ssd1306
+
+# Install python3-pil
+sudo apt-get install python3-pil
+
+# Upgrade setuptools
+sudo pip3 install --upgrade setuptools
+
+# Install adafruit-python-shell
+sudo pip3 install --upgrade adafruit-python-shell
+
+# Download the raspi-blinka.py script
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+
+# Run the script
+sudo python3 raspi-blinka.py
+
+# Check I2C and SPI devices
+ls /dev/i2c* /dev/spi*
+
+echo "Script finished successfully"
